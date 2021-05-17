8fd718ee66afab9abe5d1685d8fe49740b412df4
Albert Krzymowski <albertkrzymowski@gmail.com>
2021-05-17 22:29:32 +0200

    make max depth variable global

--- breakcrawl.go
+++ breakcrawl.go
@@ -10,9 +10,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+var maxDepth uint = 1
+
 var visited = make(map[string]bool)
 
-func visit(link *url.URL, depth uint, max uint) {
+func visit(link *url.URL, depth uint) {
 	linkStr := link.String()
 	if _, ok := visited[linkStr]; ok {
 		return
@@ -32,20 +34,20 @@ func visit(link *url.URL, depth uint, max uint) {
 
 	page.PrintPage(link, doc)
 
-	if depth < max {
+	if depth < maxDepth {
 		page.ForEachLink(link, doc, func(next *url.URL) {
-			visit(next, depth+1, max)
+			visit(next, depth+1)
 		})
 	}
 }
 
 func main() {
-	depth := flag.Uint("d", 1, "the depth of the link traversal")
+	flag.UintVar(&maxDepth, "d", 1, "the depth of the link traversal")
 	flag.Parse()
 
 	link, err := url.Parse("https://www.breakit.se")
 	if err != nil {
 		log.Fatal(err)
 	}
-	visit(link, 0, *depth)
+	visit(link, 0)
 }
